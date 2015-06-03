c9ccb28652095ce51aa58ad62959e37acf512e62
Nate Goldman <nate@ngoldman.me>
2015-06-03 16:29:03 -0700

    :sparkles: standardize

--- models/Gist.js
+++ models/Gist.js
@@ -1,70 +1,65 @@
-var Geohub = require('geohub');
+var Geohub = require('geohub')
 
-function Gist( koop ){
-
-  if ( !koop.config.ghtoken ){
-    console.warn('No Github Token in config. This may cause problems accessing data.');
+function Gist (koop) {
+  if (!koop.config.ghtoken) {
+    console.warn('No Github Token in config. This may cause problems accessing data.')
   }
 
-  var gist = {};
+  var gist = {}
 
-  gist.__proto__ = koop.BaseModel( koop );
+  gist.prototype = koop.BaseModel(koop)
 
-  gist.find = function find( id, options, callback ){
+  gist.find = function find (id, options, callback) {
+    var type = 'Gist'
+
     // looks for data in the cache first
-    var type = 'Gist';
-    koop.Cache.get( type, id, options, function(err, entry ){
-      if ( err ){
-        Geohub.gist( { id: id, token: koop.config.ghtoken }, function( err, geojson ){
-          if (err){
-            callback(err, null);
-          } else {
-            if ( !geojson.length ){
-              geojson = [ geojson ];
+    koop.Cache.get(type, id, options, function (err, entry) {
+      if (err) {
+        Geohub.gist({ id: id, token: koop.config.ghtoken }, function (err, geojson) {
+          if (err) return callback(err, null)
+
+          if (!geojson.length) geojson = [geojson]
+
+          var _totalLayer = geojson.length
+          var finalJson = []
+
+          // local method to collect layers and send them all
+          var _send = function (data) {
+            finalJson.push(data)
+
+            if (finalJson.length === _totalLayer) {
+              return callback(null, finalJson)
             }
-      
-            var _totalLayer = geojson.length,
-              finalJson = [];
-            // local method to collect layers and send them all
-            var _send = function(data){
-              finalJson.push(data);
-              if ( finalJson.length == _totalLayer ) {
-                callback(null, finalJson);
-              }
-            };
-  
-            geojson.forEach(function(layer, i){
-              koop.Cache.insert( type, id, layer, i, function( err, success){
-                if ( success ) {
-                  _send(layer);
-                } 
-              });
-            });
           }
-        });
+
+          geojson.forEach(function (layer, i) {
+            koop.Cache.insert(type, id, layer, i, function (err, success) {
+              if (err) return callback(err)
+              if (success) return _send(layer)
+            })
+          })
+        })
       } else {
-        callback( null, entry );
+        callback(null, entry)
       }
-    });
-  };
-  
+    })
+  }
+
   // compares the updated_at timestamp on the cached data and the hosted data
   // this method name is special reserved name that will get called by the cache model
-  gist.checkCache = function checkCache(id, data, options, callback){
-    var json = data;
-    Geohub.gistSha(id, koop.config.ghtoken, function(err, sha){
-      if ( sha == json[0].updated_at ){
-        callback(null, false);
-      } else {
-        Geohub.gist( { id: id, token: koop.config.ghtoken }, function( err, geojson ){
-          callback(null, geojson);  
-        });
-      }
-    });
-  };
+  gist.checkCache = function checkCache (id, data, options, callback) {
+    Geohub.gistSha(id, koop.config.ghtoken, function (err, sha) {
+      if (err) return callback(err)
+      if (sha === data[0].updated_at) return callback(null, false)
 
-  return gist;
-};
+      Geohub.gist({ id: id, token: koop.config.ghtoken }, function (err, geojson) {
+        if (err) return callback(err)
+        callback(null, geojson)
+      })
+    })
+  }
 
+  return gist
+}
 
-module.exports = Gist;
+module.exports = Gist
--- test/model-test.js
+++ test/model-test.js
@@ -1,41 +1,38 @@
-var should = require('should'),
-  koop = require('koop/lib');
+/* global before, describe, it */
 
-var repo = 'geodata',
-  user = 'chelm',
-  file = 'co-river-basin';
-  key = [repo, user, file].join('/');
+var should = require('should')
+var koop = require('koop/lib')
+var GistModel = require('../models/Gist.js')
+var Gist
 
 before(function (done) {
-  koop.Cache.db = koop.LocalDB;
-  var data_dir = __dirname + '/output/';
-  koop.Cache.data_dir = data_dir;
-  Gist = new require('../models/Gist.js')( koop );
-  done();
-});
+  var data_dir = __dirname + '/output/'
+  koop.Cache.db = koop.LocalDB
+  koop.Cache.data_dir = data_dir
+  Gist = new GistModel(koop)
+  done()
+})
 
-describe('Github Model', function(){
+describe('Github Model', function () {
+  describe('when caching a github file', function () {
+    it('should find the repo and return the data', function (done) {
+      Gist.find(6178185, {}, function (err, data) {
+        should.not.exist(err)
+        should.exist(data)
+        data.length.should.equal(2)
+        done()
+      })
+    })
 
-  describe('when caching a github file', function(){
+    it('should find the repo and return the data', function (done) {
+      Gist.checkCache(6178185, [{updated_at: 1234}], {}, function (err, data) {
+        should.not.exist(err)
+        should.exist(data)
+        data.length.should.equal(2)
+        done()
+      })
+    })
 
-    it('should find the repo and return the data', function(done){
-      Gist.find(6178185, {}, function(err, data){
-        should.not.exist(err);
-        should.exist(data);
-        data.length.should.equal(2);
-        done();
-      });
-    });
+  })
 
-    it('should find the repo and return the data', function(done){
-      Gist.checkCache(6178185, [{updated_at:1234}], {}, function(err, data){
-        should.not.exist(err);
-        should.exist(data);
-        data.length.should.equal(2);
-        done();
-      });
-    });
-
-  });
-
-});
+})
